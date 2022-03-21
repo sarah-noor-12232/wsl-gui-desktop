fb6afe146353a33a9d27e6dd9cc55ffe83594a9a
LunyCipres <88581241+LunyCipres@users.noreply.github.com>
2022-03-21 12:27:49 +0500

    Adding option by parameters
    Now you can pass parameters to launch-desktop command to select one of the four x-server window options

--- desktop.h
+++ desktop.h
@@ -1,9 +1,23 @@
 launch-desktop()
 {
-	#xserver
+	# xserver
+	# following 3 lines are taken from here: https://medium.com/@japheth.yates/the-complete-wsl2-gui-setup-2582828f4577
 	export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
 	export LIBGL_ALWAYS_INDIRECT=1
 	sudo /etc/init.d/dbus start &> /dev/null
-	cmd.exe /C C:/Users/Dell/config1.xlaunch
+		
+	#cmd.exe /C C:/Users/Dell/config1.xlaunch
+
+	if [[ "$@" == *large-window* || "$@" == *large* && "$@" == *window* ]]; then
+		cmd.exe /C C:/Users/Dell/large-window.xlaunch
+	elif [[ "$@" == *no-titlebar* || "$@" == *no* && "$@" == *titlebar* ]]; then
+		cmd.exe /C C:/Users/Dell/no-titlebar.xlaunch
+	elif [[ "$@" == *full-screen* || "$@" == *full* && "$@" == *screen* ]]; then
+		cmd.exe /C C:/Users/Dell/full-screen.xlaunch
+	else
+		# this line defines which option to choose by default
+		cmd.exe /C C:/Users/Dell/large-window.xlaunch
+	fi
+
 	mate-session
 }
